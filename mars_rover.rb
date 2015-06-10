feb7974f328849df4ed7bf29a45ccff3ffb385fc
Amar Sharma <amarsharma.hacker@gmail.com>
2015-06-10 16:57:00 +0530

    Minified and exception handled

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,89 +1,134 @@
 class Plateau
 	attr_reader :x,:y
 	def initialize(x,y)
+		raise ArgumentError, " Co-ordinates for Plateau should be positive" unless ( input_sanitization(x,y) )
 		@x = x
 		@y = y
+	end
+
+	def input_sanitization(x,y)
+		(x >= 0 && y >= 0 && (x.is_a? Numeric) && (y.is_a? Numeric))
 	end
 end
 
 class Robot
 	attr_accessor :x,:y,:heading
-	def initialize(x,y,heading)
+	attr_reader :plateau
+	def heading_check(h)
+		h == 'N' || h == 'E' || h == 'W' || h == 'S'
+	end
+
+	def initialize(x,y,heading,plateau)
+		raise ArgumentError, " Rover position Invalid!" unless ( plateau.input_sanitization(x,y) && heading_check(heading) )
+		@plateau = plateau
 		@x = x
 		@y = y
 		@heading = heading
 	end
 
-	def move_it(x,y,h)
-		case h
-			when 'N'
-				y += 1
-			when 'S'
-				y -= 1
-			when 'E'
-				x += 1
-			when 'W'
-				x -= 1
-			else
-				puts 'Error'
+	def ahead(x,y,h)
+		begin
+			x1 = x
+			y1 = y
+			case h
+				when 'N'
+					y += 1
+				when 'S'
+					y -= 1
+				when 'E'
+					x += 1
+				when 'W'
+					x -= 1
+			end
+			raise RuntimeError, " Rover went off the Plateau!" unless (plateau.input_sanitization(x,y) && heading_check(heading) && (x <= self.plateau.x && y <= self.plateau.y))
+		rescue RuntimeError
+			puts " Rover flying away! Ignored last move!"
+			return [x1,y1,h]
 		end
-		return x,y
+		[x,y,h]
 	end
 
-	def turn_right(h)
-		case h
-			when 'N'
-				h = 'E'
-			when 'S'
-				h = 'W'
-			when 'E'
-				h = 'S'
-			when 'W'
-				h = 'N'
-			else
-				puts 'Error'
+	def turn(direction,h)
+		begin
+			case direction
+				when "L"
+					h = find_new_heading(-1,h)
+				when "R"
+					h = find_new_heading(1,h)
+				else
+					raise ArgumentError, " Invalid direction!"
+			end
+		rescue ArgumentError
+			puts "Invalid direction! Rover has ignored turn"
 		end
-		h
+		[@x,@y,h]
 	end
 
-	def turn_left(h)
-		case h
-			when 'N'
-				h = 'W'
-			when 'S'
-				h = 'E'
-			when 'E'
-				h = 'N'
-			when 'W'
-				h = 'S'
-			else
-				puts 'Error'
+	def move(directions)
+		directions.each_char do |direction|
+			@x,@y,@heading = case direction
+				when "M"
+					ahead(x,y,@heading) 
+				else
+					turn(direction,@heading)
+			end
 		end
-		h
+		[@x,@y,@heading]
 	end
 
-	def move(direction,plateau)
-		x = self.x
-		y = self.y
-		h = self.heading
-		case direction
-			when 'M'
-				x,y=move_it(x,y,h) 
-			when 'L'
-				h = turn_left(h)
-			when 'R'
-				h = turn_right(h)
-			else
-				puts ' Error'
-		end
-		self.x = x if x <= plateau.x && x >= 0 #Checking bounds on X
-		self.y = y if y <= plateau.y && y >= 0 #Checking bounds on Y
-		self.heading = h
+	private
+	def find_new_heading(x,h)
+		compass = ["N","E","S","W"]
+		x = (compass.index(h) == 3 && x == 1 )? -3 : x
+		compass[compass.index(h) + x]
 	end
+end
+
+class Navigate
+	attr_accessor :plat, :rovers
+	def initialize(plateau_x,plateau_y)
+		@plat = Plateau.new(plateau_x,plateau_y)
+		@rovers = []
+	end
+
+	def insert_rover(x,y,h)
+		@rover << Robot.new(x,y,h)
+	end
+
+	def main()
+		rover = Robot.new(0,0,'N',@plat)
+		rover_inp = true
+		STDIN.each_line do |line|
+			if rover_inp
+				r = line.split(" ")
+				r_x,r_y,h = r[0].to_i,r[1].to_i,r[2]
+				rover = Robot.new(r_x,r_y,h,@plat)
+				rover_inp = false
+			else
+				x,y,h = rover.move(line.chomp)
+				puts "#{x} #{y} #{h}"
+				rover_inp = true
+			end
+		end
+		
+	end
+end
+
+if __FILE__==$0
+	
+	begin
+		plat = gets.chomp.split(" ")
+		p_x,p_y = plat[0].to_i,plat[1].to_i
+		nav = Navigate.new(p_x,p_y)
+	rescue ArgumentError
+		print 'Retry!'
+		retry
+	end
+	nav.main()
 end
 
 =begin 
  Assuming rovers can overlap each other.
  running program by this command
- ruby mars_rover.rb
+ ruby mars_rover.rb or ruby mars_rover.rb < input.txt
 =end
--- test-rover.rb
+++ test-rover.rb
@@ -4,7 +4,14 @@ require "./mars_rover"
 class Test_rover < Minitest::Test
   def setup
     @plat = Plateau.new(5,5)
-    @robot = Robot.new(1,2,'N')
+    @robot = Robot.new(1,2,'N',@plat)
+  end
+  def test_plateau_inputs
+    assert_raises(ArgumentError) {Plateau.new(5,-5)}
+    assert_raises(ArgumentError) {Plateau.new(-5,-5)}
+    assert_raises(ArgumentError) {Plateau.new(-5,5)}
+    assert_raises(ArgumentError) {Plateau.new('w',-5)}
+    assert_raises(ArgumentError) {Plateau.new(5,'e')}
   end
 
   def test_that_object_exists
@@ -17,64 +24,55 @@ class Test_rover < Minitest::Test
     assert_equal 5 , @plat.y
   end
 
+  def test_robot_inputs
+    assert_raises(ArgumentError) {Robot.new(1,-2,"N")}
+    assert_raises(ArgumentError) {Robot.new(-1,2,"N")}
+    assert_raises(ArgumentError) {Robot.new(6,-2,"N")}
+    assert_raises(ArgumentError) {Robot.new(1,6,"N")}
+    assert_raises(ArgumentError) {Robot.new('g',-2,"N")}
+    assert_raises(ArgumentError) {Robot.new(1,'x',"N")}
+    assert_raises(ArgumentError) {Robot.new(1,-2,"R")}
+    assert_raises(ArgumentError) {Robot.new(1,-2,5)}
+  end
   def test_that_robot_is_initialized
     assert_equal 1 , @robot.x
     assert_equal 2 , @robot.y
     assert_equal 'N' , @robot.heading
+    assert_equal Plateau, @robot.plateau.class
   end
 
-  def test_that_move_it_works
-    assert_equal [1,3],@robot.move_it(@robot.x,@robot.y,@robot.heading)
+  def test_that_ahead_works
+    assert_equal [1,3,'N'],@robot.ahead(@robot.x,@robot.y,@robot.heading)
   end
 
-  def test_that_turn_right_works
-    assert_equal 'E',@robot.turn_right(@robot.heading)
-    assert_equal 'S',@robot.turn_right('E')
-    assert_equal 'W',@robot.turn_right('S')
-    assert_equal 'N',@robot.turn_right('W')
-  end
+  def test_that_turn_works
+    assert_equal 'E',@robot.turn("R",@robot.heading)[2]
+    assert_equal 'S',@robot.turn("R",'E')[2]
+    assert_equal 'W',@robot.turn("R",'S')[2]
+    assert_equal 'N',@robot.turn("R",'W')[2]
 
-  def test_that_turn_left_works
-    assert_equal 'W',@robot.turn_left(@robot.heading)
-    assert_equal 'N',@robot.turn_left('E')
-    assert_equal 'E',@robot.turn_left('S')
-    assert_equal 'S',@robot.turn_left('W')
+    assert_equal 'W',@robot.turn("L",@robot.heading)[2]
+    assert_equal 'N',@robot.turn("L",'E')[2]
+    assert_equal 'E',@robot.turn("L",'S')[2]
+    assert_equal 'S',@robot.turn("L",'W')[2]
   end
 
   def test_move
-    @robot.move('L',@plat)
+    @robot.move('L')
     assert_equal [1,2,'W'],[@robot.x,@robot.y,@robot.heading]
-    @robot.move('M',@plat)
+    @robot.move('M')
     assert_equal [0,2,'W'],[@robot.x,@robot.y,@robot.heading]
-    @robot.move('R',@plat)
+    @robot.move('R')
     assert_equal [0,2,'N'],[@robot.x,@robot.y,@robot.heading]
-  end
-
-  def test_move_on_string
-    "LMRRLMRMM".each_char do |ch|
-      @robot.move(ch,@plat)
-    end
+    @robot.move("LMRRLMRMM")
     assert_equal [2,3,'E'], [@robot.x,@robot.y,@robot.heading]
-
-    "LMMMMM".each_char do |ch|
-      @robot.move(ch,@plat)
-    end
+    @robot.move("LMMMMM")
     assert_equal [2,5,'N'], [@robot.x,@robot.y,@robot.heading]
-
-    "LMMMMMMM".each_char do |ch|
-      @robot.move(ch,@plat)
-    end
+    @robot.move("LMMMMMMM")
     assert_equal [0,5,'W'], [@robot.x,@robot.y,@robot.heading]
-
-    "LMMMMMMM".each_char do |ch|
-      @robot.move(ch,@plat)
-    end
+    @robot.move("LMMMMMM")
     assert_equal [0,0,'S'], [@robot.x,@robot.y,@robot.heading]
-
-    "LMMMMMMM".each_char do |ch|
-      @robot.move(ch,@plat)
-    end
+    @robot.move("LMMMMM")
     assert_equal [5,0,'E'], [@robot.x,@robot.y,@robot.heading]
-
   end
 end
